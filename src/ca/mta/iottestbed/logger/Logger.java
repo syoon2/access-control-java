e54bb197db17be5de35af7379402214ea86960ca
Hayden Walker <haydenwalker@live.com>
2023-06-14 14:49:09 -0300

    Updated comment

--- src/ca/mta/iottestbed/logger/Logger.java
+++ src/ca/mta/iottestbed/logger/Logger.java
@@ -1,5 +1,17 @@
 package ca.mta.iottestbed.logger;
 
+/**
+ * A logger, for writing messages to.
+ * 
+ * @author Hayden Walker
+ * @version 2023-06-14
+ */
 public interface Logger {
+
+    /**
+     * Log a series of messages.
+     * 
+     * @param messages Messages to log.
+     */
     public void log(String ... messages);
 }
